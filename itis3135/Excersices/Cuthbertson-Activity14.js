cfc4baac7e8c177ec9e9299aa7a162448e651145
[dcuthbe8] <[dcuthbe8@uncc.edu]>
2021-11-20 09:13:16 -0500

    Activity 13 and 14 completed
    worked on ajax functionality and editing JavaScript files

---
+++ itis3135/Excersices/Cuthbertson-Activity14.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#nav_list li").click(function() {
+        let title = $(this).children('a').attr("title");
+        var filePath = "json_files/" + title + ".json";
+        console.log(filePath);
+
+        $.ajax({
+            type: "get",
+            url: filePath,
+            beforeSend: function() {
+                $("main").html("Loading...");
+            },
+            timeout: 10000,
+            error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function(data) {
+                console.log(data); //print data' to conso1e
+                $("main").html("");
+                $.getJSON(filePath, function(data) {
+                    $.each(data, function() {
+                        $.each(this, function(key, value) {
+                            $("main").append(
+                                "<h1>" + value.title + "</h1>" +
+                                "<h2>" + value.month + "</h2>" +
+                                "<h3>" + value.speaker + "</h3>" +
+                                "<img src=" + value.image + ">" +
+                                "<p>" + value.text + "</p>"
+                            )
+                        });
+                    });
+                });
+            }
+        }); 
+    }); 
+}); 
